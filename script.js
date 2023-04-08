963db605b784d26dc1da7090cd9e63834230de56
chandruCN <126045844+chandruCN@users.noreply.github.com>
2023-04-08 12:21:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-const ratingStars = [...document.getElementsByClassName("rating__star")];
-const ratingResult = document.querySelector(".rating__result");
-
-printRatingResult(ratingResult);
-
-function executeRating(stars, result) {
-  const starClassActive = "rating__star fas fa-star";
-  const starClassUnactive = "rating__star far fa-star";
-  const starsLength = stars.length;
-  let i;
-  //pass the parameter
-  stars.map((star) => {
-    star.onclick = () => {
-      //to find the index of the click
-      i = stars.indexOf(star);
-      // check if two values do not equal each other.
-      if (star.className.indexOf(starClassUnactive) !== -1) {
-        printRatingResult(result, i + 1);
-        for (i; i >= 0; --i) stars[i].className = starClassActive;
-      } else {
-        printRatingResult(result, i);
-        for (i; i < starsLength; ++i) stars[i].className = starClassUnactive;
-      }
-    };
-  });
-}
-
-function printRatingResult(result) {
-  result.textContent = ``;
-}
-
-executeRating(ratingStars, ratingResult);
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Star Rating</title>
+    <!-- Font Awesome Icons -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+    />
+    <!-- Google Fonts -->
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+      rel="stylesheet"
+    />
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="rating">
+      <!--span for fill the colurs when click-->
+      <span class="rating__result"></span>
+      <i class="rating__star far fa-star"></i>
+      <i class="rating__star far fa-star"></i>
+      <i class="rating__star far fa-star"></i>
+      <i class="rating__star far fa-star"></i>
+      <i class="rating__star far fa-star"></i>
+    </div>
+    <!-- Script -->
+    <script src="script.js"></script>
+  </body>
+</html>
